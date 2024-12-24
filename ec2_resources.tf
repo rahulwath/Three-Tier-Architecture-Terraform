7db9de34fe26b0fe67d101411865446858eadb04
rahul@kreditz.com <rahul@kreditz.com>
2024-12-24 15:36:33 +0530

    update ec2_resources.tf

--- ec2_resources.tf
+++ ec2_resources.tf
@@ -173,6 +173,13 @@ resource "aws_security_group" "autoscaling-sg-web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow all outbound traffic
   egress {
     from_port   = 0
@@ -198,6 +205,13 @@ resource "aws_security_group" "autoscaling-sg-app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   # Allow all outbound traffic
   egress {
     from_port   = 0
